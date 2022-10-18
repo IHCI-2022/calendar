fe3eb296442bc8954b9c22edae50408ce282e7d0
iSonce <705213278@qq.com>
2022-10-19 01:32:29 +0800

    fix(config): 修改数据库url为本地

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://root:hgc16711@8.134.49.87:27017/calendar',
+  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/calendar',
   port: process.env.PORT || 8000,
   language: 'cn',
 };
